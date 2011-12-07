8dc7d582c6c2956e56fcda0ac4e5bf0e2fc4e571
DSC-Team <ltrifonov@gmail.com>
2011-12-07 12:41:02 +0200

    BFS patch by Sergei

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 20 */
+/* This file is auto generated, version 21 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#20 PREEMPT Wed Dec 7 11:52:46 EET 2011"
-#define LINUX_COMPILE_TIME "11:52:46"
+#define UTS_VERSION "#21 PREEMPT Wed Dec 7 12:35:53 EET 2011"
+#define LINUX_COMPILE_TIME "12:35:53"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "cb-luboubuntu"
 #define LINUX_COMPILE_DOMAIN "(none)"
